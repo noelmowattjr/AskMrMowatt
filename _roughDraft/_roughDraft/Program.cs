58dd6b30118ad9129896abefab5b3479a852f1d3
Noel Mowatt <NoelMowattJr@gmail.com>
2017-02-10 15:46:39 -0500

    Created a static method that greets user according to time of day

--- _roughDraft/_roughDraft/Program.cs
+++ _roughDraft/_roughDraft/Program.cs
@@ -10,6 +10,44 @@ namespace _roughDraft
     {
         static void Main(string[] args)
         {
+            /*A photo recognition app that identifies objects in real time photos, but identifies them in French.*/
+
+            //--name prompt
+            Console.Write("Hi, what is your name: ");
+            string name = Console.ReadLine();
+
+            string greetings = timeOfDay(name);
+            Console.WriteLine(greetings);
+        }
+
+        //--Method greets user according to time of day
+        static string timeOfDay(string name)
+        {
+            //--datetime object created
+            DateTime datetime = new DateTime();
+
+            //--Instantiate time to output real time
+            datetime = DateTime.Now;
+
+            //--Conditional greetings for time of day--//
+            if (datetime.Hour >= 0 && datetime.Hour < 12)
+            {
+                Console.WriteLine("Bonjour, {0}!", name);
+            }
+            else if (datetime.Hour >= 12 && datetime.Hour < 16)
+            {
+                Console.WriteLine("Bonne apres-midi, {0}!", name);
+            }
+            else if (datetime.Hour >= 16 && datetime.Hour < 19)
+            {
+                Console.WriteLine("Bonsoir, {0}!", name);
+            }
+            else if (datetime.Hour >= 20)
+            {
+                Console.WriteLine("Bonne nuit, {0}!", name);
+            }
+
+            return "";
         }
     }
 }
